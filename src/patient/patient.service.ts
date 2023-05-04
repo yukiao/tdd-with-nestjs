3c789c97e30d0dc521de370f5c740e711f85f16a
Sony Lembang <54715022+yukiao@users.noreply.github.com>
2023-05-04 19:52:06 +0800

    feat: 🎸 add test to check if user with given id exist

--- src/patient/patient.service.ts
+++ src/patient/patient.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Patient } from 'src/patient/patient.model';
 
+export interface PatientInput {
+    name: string;
+}
 @Injectable()
 export class PatientService {
-    async register(patientInput: any): Promise<any> {
+    async register(patientInput: PatientInput): Promise<Patient> {
         return {
             id: 1,
             name: patientInput.name
         }
+    }
+
+    public async doesPatientExist(patientId: number): Promise<boolean>{
+        return false
     }
 }
